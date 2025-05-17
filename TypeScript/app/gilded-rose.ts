9ae1385cb8d97e283bc34e598e6917839408f8da
BertVereecken <bert@codiver.be>
2025-05-17 19:32:08 +0200

    kaizen: extract some logic to the updateBackStagePassQuality method

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -14,19 +14,14 @@ export class GildedRose {
         return;
       }
 
-      if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-        this.decreaseQuality(item)
-      } else if (item.quality < 50) {
-        this.increaseQuality(item)
+      if (this.isBackStagePass(item)) {
+        this.updateBackStagePassQuality(item);
+      }
 
-        if (this.isBackStagePass(item)) {
-          if (item.sellIn < 11) {
-            this.increaseQuality(item)
-          }
-          if (item.sellIn < 6) {
-            this.increaseQuality(item)
-          }
-        }
+      if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+        this.decreaseQuality(item);
+      } else if (item.quality < 50) {
+        this.increaseQuality(item);
       }
 
       this.decreaseSellin(item);
@@ -34,11 +29,7 @@ export class GildedRose {
       if (item.sellIn < 0) {
         if (this.isAgedBrie(item)) {
           this.increaseQuality(item);
-        } else if (this.isBackStagePass(item)) {
-          item.quality = item.quality - item.quality
-        } else {
-          this.decreaseQuality(item)
-        }
+        } else this.decreaseQuality(item)
       }
     });
 
@@ -72,5 +63,18 @@ export class GildedRose {
 
   private decreaseSellin(item: Item): void {
     item.sellIn -= 1;
+  }
+
+  private updateBackStagePassQuality(item: Item) {
+    if (item.sellIn < 11) {
+      this.increaseQuality(item)
+    }
+    if (item.sellIn < 6) {
+      this.increaseQuality(item)
+    }
+
+    if (item.sellIn < 0) {
+      item.quality = 0;
+    }
   }
 }
