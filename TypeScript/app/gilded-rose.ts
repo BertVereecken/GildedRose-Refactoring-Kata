4d1cceec39bf3d04172a9f66a38ab64e0999f67a
BertVereecken <bert@codiver.be>
2025-05-17 19:15:00 +0200

    kaizen: extract if logic into method

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -15,10 +15,7 @@ export class GildedRose {
       }
 
       if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (item.quality > 0) {
-            item.quality = item.quality - 1
-        }
-
+        this.decreaseQuality(item)
       } else if (item.quality < 50) {
         item.quality = item.quality + 1
         if (this.isBackStagePass(item)) {
@@ -35,13 +32,11 @@ export class GildedRose {
 
       if (item.sellIn < 0) {
         if (this.isAgedBrie(item)) {
-            this.increaseQuality(item);
+          this.increaseQuality(item);
         } else if (this.isBackStagePass(item)) {
           item.quality = item.quality - item.quality
         } else {
-          if (item.quality > 0) {
-            this.decreaseQuality(item)
-          }
+          this.decreaseQuality(item)
         }
       }
     });
@@ -69,7 +64,9 @@ export class GildedRose {
   }
 
   private decreaseQuality(item: Item): void {
+    if (item.quality > 0) {
       item.quality -= 1;
+    }
   }
 
   private decreaseSellin(item: Item): void {
