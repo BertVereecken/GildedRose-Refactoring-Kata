838d013e40ad910bd4aadf71ab739133d39d6f76
BertVereecken <bert@codiver.be>
2025-05-17 14:13:39 +0200

    kaizen: improve test a bit. Asserting the whole class

--- TypeScript/test/vitest/gilded-rose.spec.ts
+++ TypeScript/test/vitest/gilded-rose.spec.ts
@@ -3,7 +3,9 @@ import { Item, GildedRose } from '@/gilded-rose';
 describe('Gilded Rose', () => {
   it('should foo', () => {
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('foo');
+
+    expect(items[0]).toEqual<Item>(new Item('foo', -1, 0));
   });
 });
